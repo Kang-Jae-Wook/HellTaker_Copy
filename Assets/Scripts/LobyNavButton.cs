e7ef02bedbf79c76b2279185005a4f26c165f43d
Kang-Jae-Wook <82699453+Kang-Jae-Wook@users.noreply.github.com>
2022-05-11 23:43:25 +0900

    SetReset

--- Assets/Scripts/LobyNavButton.cs
+++ Assets/Scripts/LobyNavButton.cs
@@ -10,13 +10,16 @@ public class LobyNavButton : MonoBehaviour
     private bool EndBtn;
     private GameObject ButtonOnOff;
     private GameObject EndOnOff;
-    private GameObject ImgSet;
+    private GameObject BeelImg;
     private GameObject StartTexts;
+    private GameObject StartImg;
+    private GameObject StartBack;
 
-    public void OnClickButton()
+    private void OnClickButton()
     {
         if (StartBtn == true)
         {
+            ButtonOnOff = GameObject.Find("ButtonCanvas");
             if (Input.GetKeyDown(KeyCode.DownArrow))
             {
                 editBtnValue(false, true, false);
@@ -25,7 +28,8 @@ public class LobyNavButton : MonoBehaviour
                 EndBtn = false;*/
                 Debug.Log("시작에서챕터");
             }
-        }else if(ChaptBtn == true)
+        }
+        else if(ChaptBtn == true)
             {
             if (Input.GetKeyDown(KeyCode.DownArrow))
             {
@@ -54,29 +58,15 @@ public class LobyNavButton : MonoBehaviour
                 //EndBtn = false;
                 Debug.Log("종료에서챕터");
             }
-        }
-
-
-
-        if(Input.GetKeyDown(KeyCode.Return))
-        {
-            if(StartBtn == true)
+            if (Input.GetKeyDown(KeyCode.Return))
             {
-
-            }
-            else if (ChaptBtn == true)
-            {
-
-            }
-            else if (EndBtn == true)
-            {
-                ButtonOnOff = GameObject.Find("ButtonCanvas");      //  버튼포함캔버스
-                ButtonOnOff.SetActive(false);
                 EndOnOff.SetActive(true);
-                Application.Quit();
+                ButtonOnOff.SetActive(false);
             }
         }
+
     }
+
 
     private void editBtnValue(bool vSBtn, bool vCBtn, bool vEBtn) // 버튼값변경 함수
     {
@@ -92,11 +82,13 @@ public class LobyNavButton : MonoBehaviour
         ChaptBtn = GameObject.Find("ChapterButton");        //  챕터버튼
         EndBtn = GameObject.Find("EndButton");              //  종료버튼
         EndOnOff = GameObject.Find("EndTextCanvas");        //  종료클릭시 텍스트 캔버스
-        ImgSet = GameObject.Find("ImgSet");                 //  이미지총캔버스 (수정필요)
         StartTexts = GameObject.Find("StartReTextCanvas");  //  스타트버튼 캔버스
+        StartImg = GameObject.Find("StartImg1Can");
+        StartBack = GameObject.Find("StIBackCanvas");
         EndOnOff.SetActive(false);
-        ImgSet.SetActive(false);
         StartTexts.SetActive(false);
+        StartImg.SetActive(false);
+        StartBack.SetActive(false);
         StartBtn = true;
         ChaptBtn = false;
         EndBtn = false;
